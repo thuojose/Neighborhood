82dcd60e346e4becef366571912086798fe5f15b
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-31 21:19:32 +0300

    created urls.py

---
+++ watch/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name = 'Index'),
+    
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
