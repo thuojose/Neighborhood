d8e83de6fe1ff420c2865b270e5ef803805ec31a
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-31 21:10:05 +0300

    added function for index in views.py

--- watch/views.py
+++ watch/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user = request.user
+        profile = Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+
+    return render(request, 'index.html')
