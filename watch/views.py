4823cdc12a403554a978a164db4909058d8ba3d1
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-31 23:44:36 +0300

    added function to create profile

--- watch/views.py
+++ watch/views.py
@@ -90,3 +90,25 @@ def my_profile(request):
     profile = Profile.objects.get(username = current_user)
 
     return render(request, 'user_profile.html', {"profile":profile})
+
+@login_required(login_url='/accounts/login/')
+def user_profile(request, username):
+    user = User.objects.get(username = username)
+    profile = Profile.objects.get(username = user)
+
+    return render(request, 'user_profile.html', {"profile":profile})
+
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user=request.user
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.username = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+
+    else:
+        form = ProfileForm()
+    return render(request, 'profile_form.html', {"form":form})
