980fc281b7f516a028d15a5e24ba9bde372f8fd3
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-11-01 00:10:30 +0300

    created notification_form.html

--- watch/models.py
+++ watch/models.py
@@ -124,3 +124,9 @@ class BlogPost(models.Model):
     def search_blogpost(cls, search_term):
         blogs = cls.objects.filter(Q (username__username=search_term) | Q (neighbourhood__neighbourhood=search_term) | Q (title__icontains=search_term))
         return blogs
+    
+    
+class Comment(models.Model):
+    comment = models.CharField(max_length=300)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
