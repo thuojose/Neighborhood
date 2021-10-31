86264d3be6c69ad1092a4a45159acbda07ed4fb3
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-10-31 22:10:14 +0300

    added class for health in models.py

--- watch/models.py
+++ watch/models.py
@@ -32,3 +32,30 @@ class Neighbourhood(models.Model):
         self.neighbourhood_name = neighbourhood_name
         self.save()
 
+
+class notifications(models.Model):
+    title = models.CharField(max_length=100)
+    notification = HTMLField()
+    priority = models.CharField(max_length=15, choices=Priority, default="Informational")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE
+    )
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class healthservices(models.Model):
+    healthservices = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.healthservices
+
+    def save_healthservices(self):
+        self.save()
+
+    @classmethod
+    def delete_healthservices(cls, healthservices):
+        cls.objects.filter(healthservices=healthservices).delete()
+
